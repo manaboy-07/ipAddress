705ffaf9fda8be19d13f57e1f7f5d364a99ee8aa
manaboy-07 <manassehoruebor@gmail.com>
2023-08-13 22:35:27 +0100

    trying to get address

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Search from './components/Search';
-
+import { fetchIpAddress } from './utils/fetchIpAddress';
 function App() {
   const [ipValue, setIpValue] = useState('')
+  const [results, setResults] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(ipValue){
+        console.log(`Your IP address is ${ipValue}`)
+        useEffect(() => {
+          fetchIpAddress(ipValue).then(data => console.log(data))
+        }, [ipValue])
+        
+    }else{
+        alert('No IP address is inputed')
+    }
+}
+  
+  
   return (
     <div className="flex items-center justify-center m-3 p-3">
       <h2 className="text-white text-2xl">IP Address Tracker</h2>
-      <Search ipValue={ipValue} setIpValue={setIpValue}/>
+      <Search ipValue={ipValue} setIpValue={setIpValue} handleSubmit={handleSubmit}/>
     </div>
   );
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Arrow from '../images/icon-arrow.svg'
-export default function Search() {
+export default function Search({ipValue , setIpValue}) {
+    
   return (
     <div className='bg-white p-3 m-2 flex flex-row text-xl'>
-      <input type="text" placeholder='Search for any IP address or domain'/>
-      <div className='bg-black text-white'>
-        <img src={Arrow} alt="" />
-      </div>
+      <form onSubmit={handleSubmit}>
+      <input type="text" placeholder='Search for any IP address or domain' value={ipValue} onChange={(e) => setIpValue(e.target.value)}/>
+      <button className='bg-black text-white p-2 btn'>
+        <img src={Arrow} alt="" className='w-6 h-6'/>
+      </button>
+      </form>
     </div>
   )
 }
